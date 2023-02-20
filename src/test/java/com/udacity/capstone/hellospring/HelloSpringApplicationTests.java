66eeb34645190dbb3b1b90f06e7bc10a775f96f7
Cardo Eggert <eggert@rushstreetinteractive.com>
2023-02-20 20:30:55 +0200

    Fix test and change circle ci pipeline

--- src/test/java/com/udacity/capstone/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/com/udacity/capstone/hellospring/HelloSpringApplicationTests.java
@@ -18,7 +18,7 @@ class HelloSpringApplicationTests {
 
   @Test
   void helloControllerTest_Amy() {
-    assertThat(helloController.hello("Amy")).isEqualTo("Hello Amy1!");
+    assertThat(helloController.hello("Amy")).isEqualTo("Hello Amy!");
   }
 
 }
