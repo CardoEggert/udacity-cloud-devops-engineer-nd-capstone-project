f626c6055d6fee610879632e399ebfe721446ba1
Cardo Eggert <eggert@rushstreetinteractive.com>
2023-02-25 21:15:38 +0200

    Revert "Break linting"
    This reverts commit 593c381ed6e76d17e884e6c4ea69e613276b7235.

--- src/main/java/com/udacity/capstone/hellospring/HelloController.java
+++ src/main/java/com/udacity/capstone/hellospring/HelloController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/v2/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return "Hello %s!".formatted(name);
-    }
+  @GetMapping("/v2/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return "Hello %s!".formatted(name);
+  }
 }
